db46868202cc25c1b837f947deefdda0bac2cf5f
LATIM RONNIE DAVID <89303631+latrondav@users.noreply.github.com>
2023-12-12 10:18:49 +0300

    Update settings.py

--- Backend/NsawoRCCMCProject/settings.py
+++ Backend/NsawoRCCMCProject/settings.py
@@ -49,18 +49,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'NsawoRCCMCApp'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = False  # Set to True for development (allows all origins)
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    # ... other allowed origins
+]
+
 
 ROOT_URLCONF = 'NsawoRCCMCProject.urls'
 
